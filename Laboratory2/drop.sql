7ca000db8f5a55120ad6ee94b201bd727d17ce1b
Kateryna <k.kolobaieva-2022@kpi.ua>
2019-10-25 17:15:39 +0300

    Create drop.sql

---
+++ Laboratory2/drop.sql
@@ -0,0 +1,13 @@
+DROP TABLE "Users";
+
+DROP TABLE "Events";
+
+DROP TABLE "Options";
+
+DROP TABLE "user_event";
+
+DROP TABLE "event_options";
+
+DROP TABLE "clothes_event";
+
+DROP TABLE "clothes";
