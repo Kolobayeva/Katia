8c4af98eeba964cdf49a7d85336e3f5190a71684
Kateryna <k.kolobaieva-2022@kpi.ua>
2019-10-25 15:37:54 +0300

    Add files via upload

---
+++ Laboratory2/forms/clothes.py
@@ -0,0 +1,39 @@
+from wtforms import StringField, DateTimeField, SubmitField, validators,IntegerField
+from flask_wtf import FlaskForm
+from domain import models
+from uuid import UUID
+from domain.layout import data_types
+
+
+class ClothesViewModel(FlaskForm):
+  
+     style_name = StringField(" style_name: ", [
+        validators.DataRequired("Please enter your   style_name.")
+
+    ])
+	 outwear= StringField("outwear: ", [
+        validators.DataRequired("Please enter your  outwear.")
+
+    ])
+
+    lowerwear= IntegerField("lowerwear: ", [
+        validators.DataRequired("Please enter your lowerwear.")
+
+    ])
+	
+	shoes= IntegerField("shoes: ", [
+        validators.DataRequired("Please enter your shoes.")
+
+    ])
+	
+	
+    submit = SubmitField("Save")
+	
+	def domain(self):
+        return models.Clothes(
+             style_name=self.style_name.data,
+             outwear=self.outwear.data,
+			 lowerwear=self.lowerwear.data,
+			 shoes=self.shoes.data
+          
+        )
